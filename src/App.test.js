5bb7dd8d6653070e4dc48eb38b8242c637a7c45e
kmcin <i19700101@outlook.com>
2024-09-20 23:12:56 +1000

    change test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react'; // waitFor 임포트 추가
 import App from './App';
 
 test('renders Tesla station information', async () => {
   render(<App />);
 
-  // 예를 들어 South Melbourne, VIC가 이름으로 포함된 Popup이 있는지 확인
+  
+  // "South Melbourne, VIC"가 포함된 Popup이 있는지 확인
   await waitFor(() => {
-    const stationName = screen.findByText(/South Melbourne, VIC/i);
+    const stationName = screen.getByText(/South Melbourne, VIC/i); // getByText 사용
     expect(stationName).toBeInTheDocument();
-  }); 
+  });
 
-   // "Status: OPEN" 텍스트가 렌더링될 때까지 기다립니다.
+  // 스테이션 상태가 "OPEN"인지를 확인
   await waitFor(() => {
     const status = screen.getByText(/Status: OPEN/i);
     expect(status).toBeInTheDocument();
-    });
+  });
 
-  // 충전소 개수가 렌더링될 때까지 기다립니다.
+  // 충전소 개수 확인
   await waitFor(() => {
     const stallCount = screen.getByText(/Number of charging stations:/i);
     expect(stallCount).toBeInTheDocument();
-   });
+  });
 });
