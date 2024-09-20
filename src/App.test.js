58c4274bf9f0b8c1555615e337e61b483523c842
kmcin <i19700101@outlook.com>
2024-09-20 19:32:25 +1000

    test upddate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Tesla station information', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // 예를 들어 Ashburn, VA가 이름으로 포함된 Popup이 있는지 확인
+  const stationName = await screen.findByText(/Ashburn, VA/i);
+  expect(stationName).toBeInTheDocument();
+
+  // 스테이션 상태가 "OPEN"인지를 확인
+  const status = await screen.findByText(/Status: OPEN/i);
+  expect(status).toBeInTheDocument();
+
+  // 충전소 개수 확인
+  const stallCount = await screen.findByText(/Number of charging stations:/i);
+  expect(stallCount).toBeInTheDocument();
 });
